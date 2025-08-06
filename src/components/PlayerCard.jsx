7b0b26255a5584d99bc85355fff0248d3ee79d9f
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-08-06 15:43:01 -0400

    Add styling for player card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,16 +1,20 @@
 function PlayerCard({ player }) {
   return (
-    <div>
-      <h2>{player.longName}</h2>
+    <div className="bg-zinc-900 w-100 h-120 text-white rounded-lg text-xl flex flex-col m-2 select-none">
+      <div className="bg-zinc-950 p-3 w-100 text-3xl rounded-t-lg text-center"><h2>{player.longName.toUpperCase()}</h2></div>
       <img
         src={player.headshot}
         alt={player.longName}
+        draggable="false"
         onError={(e) => {
           e.currentTarget.src = "https://a.espncdn.com/i/headshots/nophoto.png";
         }}
+        className="w-100 h-72"
       ></img>
-      <h3>{player.team}</h3>
-      <h3>{player.position}</h3>
+      <div className="bg-zinc-950 flex-1 rounded-b-lg p-7 h-auto">
+        <h3>{player.team}</h3>
+        <h3>{player.position}</h3>
+      </div>
     </div>
   );
 }
